107df46367688944c77794d60c0b24a9882c161d
Etienne Walch <tinouclt@gmail.com>
2018-12-19 21:41:49 +0100

    bugfix formulaire de connexion + redirection

--- src/Controller/AccountController.php
+++ src/Controller/AccountController.php
@@ -10,6 +10,7 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
 class AccountController extends AbstractController
 {
@@ -27,9 +28,14 @@ class AccountController extends AbstractController
      * @Route("/login", name="account_login")
      * @return Response
      */
-    public function login()
+    public function login(AuthenticationUtils $utils)
     {
-        return $this->render('account/login.html.twig');
+        $error = $utils->getLastAuthenticationError();
+        $username = $utils->getLastUsername();
+        return $this->render('account/login.html.twig',[
+            'hasError' => $error !== null,
+            'username' => $username
+        ]);
     }
 
     /**
